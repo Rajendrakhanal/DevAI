ebb58743c18a71363c1388f0a04d344952c18771
Rajendra Khanal <khanal.rajendara111@gmail.com>
2023-12-23 10:43:12 +0545

    Completion of adding code snippet

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,22 @@
 import * as vscode from "vscode";
+import { createProject } from "./projects";
+
+import boilerplatecode from "./json/boilerplatecode.json";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "vsus" is now active!');
 
-  /**
-   * * Creating and Registering the command
-   */
-  const helloWorld = vscode.commands.registerCommand("vsus.helloWorld", () => {
-    vscode.window.showInformationMessage(
-      "Hello World from VS code User Snippets!"
-    );
+  let helloWorld = vscode.commands.registerCommand("vsus.helloWorld", () => {
+    vscode.window.showInformationMessage("Hello World from VSUS!");
+    console.log(vscode.window.activeTextEditor?.document.fileName);
   });
 
-  /**
-   * TODO: Auto Completion of Boiler Plate Code
-   * @Rajendra
-   * @Ujjwal
-   * @Saurav
-   */
+  const c = getLanguageCompletion("c");
+  const cpp = getLanguageCompletion("cpp");
+  const java = getLanguageCompletion("java");
+  const go = getLanguageCompletion("go");
+  const csharp = getLanguageCompletion("csharp");
 
-  /**
-   * TODO: Notification Sample
-   * @Susheel
-   */
   const showInfoNotification = vscode.commands.registerCommand(
     "vsus.showInfoNotification",
     () => {
@@ -42,12 +36,70 @@ export function activate(context: vscode.ExtensionContext) {
     }
   );
 
+  const cProject = vscode.commands.registerCommand(
+    "vsus.createCProject",
+    () => {
+      createProject("c");
+    }
+  );
+
+  const cppProject = vscode.commands.registerCommand(
+    "vsus.createCPPProject",
+    () => {
+      createProject("cpp");
+    }
+  );
+
+  const reactProject = vscode.commands.registerCommand(
+    "vsus.createReactProject",
+    () => {
+      createProject("react");
+    }
+  );
+
+  const djangoProject = vscode.commands.registerCommand(
+    "vsus.createdjangoProject",
+    () => {
+      createProject("django");
+    }
+  );
+
+  const nodeProject = vscode.commands.registerCommand(
+    "vsus.createNodeProject",
+    () => {
+      createProject("node");
+    }
+  );
+
   context.subscriptions.push(
+    cppProject,
+    nodeProject,
+    cProject,
+    reactProject,
+    djangoProject,
     helloWorld,
     showErrorNotification,
     showInfoNotification,
     showWarningNotification
   );
 }
+
+const getLanguageCompletion = (
+  language: "c" | "cpp" | "java" | "csharp" | "go"
+): vscode.Disposable => {
+  return vscode.languages.registerCompletionItemProvider(language, {
+    provideCompletionItems() {
+      const snippetCompletion = new vscode.CompletionItem(
+        boilerplatecode[language].prefix
+      );
+
+      let body = boilerplatecode[language].body.join("");
+
+      snippetCompletion.insertText = new vscode.SnippetString(body);
+
+      return [snippetCompletion];
+    },
+  });
+};
 
 export function deactivate() {}
